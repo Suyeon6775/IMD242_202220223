5c74f67a854bb802177a368da98e5bd30711442b
“Suyeon6775” <163228437+Suyeon6775@users.noreply.github.com>
2024-11-20 18:38:41 +0900

    10

--- w10_hw/assignment/sketch.js
+++ w10_hw/assignment/sketch.js
@@ -1,7 +1,122 @@
+// 전역 변수
+let colours = ['#257180', '#F2E5BF', '#FD8B51', '#CB6040'];
+
+let gravity = [0, 0.1];
+let friction = 0.99;
+let cnt = 0;
+let mouse = [0, 0];
+let confetties = [];
+
+// 클래스 정의
+class Confetti {
+  constructor(x, y, w, h, colour, force, angForce) {
+    this.pos = [x, y];
+    this.vel = [0, 0];
+    this.size = [w, h];
+    this.colour = colour;
+
+    let randomDir = radians(random(360));
+    this.vel[0] = force * cos(randomDir);
+    this.vel[1] = force * sin(randomDir);
+
+    this.ang = radians(random(360));
+    this.angVel = radians(angForce);
+  }
+
+  update(force, friction) {
+    for (let idx = 0; idx < 2; idx++) {
+      this.vel[idx] += force[idx];
+      this.pos[idx] += this.vel[idx];
+      this.vel[idx] *= friction;
+    }
+
+    this.ang += this.angVel;
+    this.angVel *= friction;
+  }
+
+  display() {
+    push();
+    rectMode(CENTER);
+    translate(this.pos[0], this.pos[1]);
+    rotate(this.ang);
+    noStroke();
+    fill(this.colour);
+    rect(0, 0, this.size[0], this.size[1]);
+    pop();
+  }
+
+  getDiagonal() {
+    return sqrt(pow(this.size[0] * 0.5, 2) + pow(this.size[1] * 0.5, 2));
+  }
+
+  isOutOfScreen() {
+    return (
+      this.pos[0] < -this.getDiagonal() ||
+      this.pos[0] > width + this.getDiagonal() ||
+      this.pos[1] < -this.getDiagonal() ||
+      this.pos[1] > height + this.getDiagonal()
+    );
+  }
+}
+
+// 유틸리티 함수
+function gen(x, y, n) {
+  for (let idx = 0; idx < n; idx++) {
+    let randomW = random(4, 20);
+    let randomH = random(4, 20);
+    let randomForce = random(1, 10);
+    let randomAngForce = random(-30, 30);
+    let newConfetti = new Confetti(
+      x,
+      y,
+      randomW,
+      randomH,
+      colours[int(random(colours.length))],
+      randomForce,
+      randomAngForce
+    );
+    confetties.push(newConfetti);
+  }
+}
+
+// p5.js 기본 함수
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed) {
+    cnt++;
+  }
+  background(255);
+
+  for (let idx = confetties.length - 1; idx >= 0; idx--) {
+    let aConfetti = confetties[idx];
+    aConfetti.update(gravity, friction);
+    if (aConfetti.isOutOfScreen()) {
+      confetties.splice(idx, 1);
+    }
+  }
+
+  for (let idx = 0; idx < confetties.length; idx++) {
+    confetties[idx].display();
+  }
+}
+
+// 이벤트 핸들러
+function mousePressed() {
+  cnt = 0;
+  mouse[0] = mouseX;
+  mouse[1] = mouseY;
+}
+
+function mouseReleased() {
+  console.log('gen: ' + cnt);
+  gen(mouse[0], mouse[1], cnt);
+}
+
+function keyPressed() {
+  if (key === 'p' || key === 'P') {
+    console.log('confetties: ' + confetties.length);
+  }
 }
